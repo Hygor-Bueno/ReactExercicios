d86a03ceaa491917fcbc43e0032175294435f9fd
Hygor Bueno <hygor_knd.x1@hotmail.com>
2022-07-11 15:00:25 -0300

    Exportação de multiplos componentes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 // import Primeiro from "./components/Primeiro"
-import BomDia from './components/BomDia'
-ReactDom.render(<BomDia nome="Hygor" />,document.getElementById('root'));
+// import BomDia from './components/BomDia'
+import Multi,{ BoaNoite } from './components/Multiplos'
+ReactDom.render(
+    <div>
+        <Multi.BoaTarde nome="ANA" />
+        <BoaNoite nome="BIA" />
+    </div>, 
+    document.getElementById('root')
+);
