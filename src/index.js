533507bbcfb36745b9dc450c34738a2102e4b822
Hygor Bueno <hygor_knd.x1@hotmail.com>
2022-07-11 15:31:27 -0300

    Componentes com classes

--- src/index.js
+++ src/index.js
@@ -2,11 +2,19 @@ import React from 'react';
 import ReactDom from 'react-dom';
 // import Primeiro from "./components/Primeiro"
 // import BomDia from './components/BomDia'
-import Multi,{ BoaNoite } from './components/Multiplos'
+// import Multi,{ BoaNoite } from './components/Multiplos'
+import Saudacao from './components/Saudacao'
+
 ReactDom.render(
     <div>
-        <Multi.BoaTarde nome="ANA" />
-        <BoaNoite nome="BIA" />
+        <Saudacao tipo="Bom Dia" nome="Maria"/>
     </div>, 
     document.getElementById('root')
 );
+// ReactDom.render(
+//     <div>
+//         <Multi.BoaTarde nome="ANA" />
+//         <BoaNoite nome="BIA" />
+//     </div>, 
+//     document.getElementById('root')
+// );
