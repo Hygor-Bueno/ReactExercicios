d7ffa5e0f56ff44390c01a00ddd01f19a6e41d13
Hygor Bueno <hygor_knd.x1@hotmail.com>
2022-07-11 15:43:09 -0300

    Componentes com classes

--- src/components/Saudacao.jsx
+++ src/components/Saudacao.jsx
@@ -4,22 +4,26 @@ export default class Saudacao extends Component {
         tipo:this.props.tipo,
         nome:this.props.nome
     }
+    constructor(props){
+        super(props);
+        this.setTipo = this.setTipo.bind(this);
+    }
     render() {
         const {tipo,nome} = this.state;
         return (
             <div>
                 <h1>{tipo} {nome}</h1>
                 <hr/>
-                <input type="text" placeholder="Tipo..." value ={tipo} onChange={e=>this.setTipo(e)}/>
+                <input type="text" placeholder="Tipo..." value ={tipo} onChange={this.setTipo}/>
                 <input type="text" placeholder="Nome..." value ={nome} onChange={e=>this.setNome(e)}/>
             </div>
         )
     }
     setTipo(e){
-        this.setState({tipo:e.target.value})
+        this.setState({tipo:e.target.value});
         // console.log(e.target.value)
     }
     setNome(e){
-        this.setState({nome:e.target.value})
+        this.setState({nome:e.target.value});
     }
 }
